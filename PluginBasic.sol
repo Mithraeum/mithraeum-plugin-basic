223b89343f75667e3097d9c072ca5ccf0ce92ecc
Roman <33402067+RomanG11@users.noreply.github.com>
2022-08-18 19:20:57 +0300

    Create PluginBasic.sol

---
+++ PluginBasic.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.6;
+
+abstract contract PluginBasic {
+    
+    string content;
+
+    event NewContent(string newContent);
+
+    /**
+     * @dev
+	 * function must return ipfs hash. Example: QmNxdLEHJyYmaPPkMur3ua87zSVF8Wi2PWn8wBNd6A9QH1
+	 */
+    function getContent() virtual external view returns(string memory) {
+        return content;
+    }
+
+    function _setContent(string memory _content) internal {
+    	require(bytes(content).length == 0, "already set");
+        content = _content;
+        emit NewContent(_content);
+    }
+}
+
+contract Plugin is PluginBasic {
+    function setContent(string memory _content) public {
+        _setContent(_content);
+    }
+}
